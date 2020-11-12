2e2d91c748e19ac98fda31331cfdbeae6e6ea151
Brandon Boras <brandon.boras@mymail.champlain.edu>
2020-11-12 11:33:10 -0500

    adding the main file, tested, make file seems to be working

---
+++ src/Lab10.2.cpp
@@ -0,0 +1,22 @@
+
+/**
+* @file Lab10.2.cpp
+* @brief  Week 10, First steps with C++ afte the previous warmup
+*
+* @author Brandon Boras
+* @assignment Lecture 10.2
+* @date 11/12/2020
+* @credits
+* original makefile setup based on prof's github
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Lab 10.2\n";
+    return EXIT_SUCCESS;
+}
