df869a33bf949b4359a8c7e38c3e82f6fedb944e
Brandon Boras <34067374+Vurglesplat@users.noreply.github.com>
2020-12-14 16:55:00 -0500

    Update TreeNavigator.cpp

--- FinalProject-WebScraper/src/TreeNavigator.cpp
+++ FinalProject-WebScraper/src/TreeNavigator.cpp
@@ -1,1 +1,29 @@
+/**
+* @file TreeNavigator.cpp
+* @brief  Searches through an XML file as a tree to  
+* to find data within html wrappers
+* @author Brandon Boras
+* @assignment Final Project
+* @date 12/14/2020
+* @credits
+* 
+*/
+
+#include "dataHandler.h"
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <cstring>
+
+                    #include <iostream>
+
+void PrintSection(string sectionName, string filePath)
+{
+    // remove the header and the html wrapper
+    // add a BIIIG wrapper to the whole thing
+    string systemCommand = "xmllint --xpath \"//head/title/text()\" Scrapes/example.xml";
+    cout << "sys command = " << systemCommand << endl;
+    int exitcode = system(systemCommand.c_str());
+}
 
